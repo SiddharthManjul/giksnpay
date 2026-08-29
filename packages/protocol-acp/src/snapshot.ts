65f78da4f20866f7c7f2a05960d2d7ce7427d678
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-08-29 11:56:05 +0530

    feat(acp): update snapshot.ts

---
+++ packages/protocol-acp/src/snapshot.ts
@@ -0,0 +1,10 @@
+export const ACP_VERSION = "2026-04-17" as const;
+
+export const ACP_PINNED_COMMIT = "7fdd78df677a94dce04c770644b0fbbb1401272b" as const;
+
+export const ACP_RELEASE_COMMIT = "9abf303f48088d170503a54502d612b8b1997897" as const;
+
+export const ACP_RELEASED_PATH_LAST_MODIFIED_COMMIT =
+  "17adf494cf8b4bcb41967a0386188b8103316d2f" as const;
+
+export const ACP_SNAPSHOT_PATH = "protocol/acp/2026-04-17" as const;
