b5ac5cc084126b70bfd04ea8bf0057c1fe68f373
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-08-30 02:44:47 +0530

    refactor(config): update index

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -2,9 +2,13 @@ export {
   type GatewayAuthEnvironment,
   gatewayAuthEnvironmentSchema,
   parseGatewayAuthEnvironment,
+  parseSignalWorksEnvironment,
   parseWorkerEnvironment,
   runtimeEnvironmentSchema,
   type RuntimeEnvironment,
+  type SignalWorksEnvironment,
+  signalWorksEnvironmentSchema,
+  signalWorksKeyEncryptionSecretSchema,
   type WorkerEnvironment,
   workerEnvironmentSchema,
 } from "./environment";
